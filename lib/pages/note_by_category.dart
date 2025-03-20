994d3b944e20f2cb15882eee7478a2d9fca87337
SandulRenuja <155486294+SandulRenuja@users.noreply.github.com>
2025-03-21 00:52:53 +0530

    Create note category card

--- lib/pages/note_by_category.dart
+++ lib/pages/note_by_category.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/services/note_services.dart';
+import 'package:notes_app/utils/constant.dart';
+import 'package:notes_app/utils/router.dart';
+import 'package:notes_app/utils/text_styles.dart';
+import 'package:notes_app/widgets/note_category_card.dart';
 
 class NoteByCategory extends StatefulWidget {
 
@@ -31,9 +35,51 @@ class _NoteByCategoryState extends State<NoteByCategory> {
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold( 
       appBar: AppBar(
-        title: Text(widget.category),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed:() {
+            //go to the notes page
+            AppRouter.router.push("/notes");
+          }, 
+          icon: Icon(Icons.arrow_back,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: AppConstants.kDefaultPadding,
+          ),
+          child: Column(
+            children: [
+              SizedBox(height: 30,),
+              Text(
+                widget.category,
+                style:AppTextStyles.appTitle,
+              ),
+              SizedBox(height: 30,),
+              GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: AppConstants.kDefaultPadding,
+                  mainAxisSpacing: AppConstants.kDefaultPadding,
+                  childAspectRatio: 7/11,
+                  ), 
+                  itemCount: noteList.length,
+                itemBuilder: (context, index) {
+                  return NoteCategoryCard(
+                    noteTitle: noteList[index].title, 
+                    noteContent: noteList[index].content, 
+                    removeNote:() {}, 
+                    editNote: () {},
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/widgets/note_category_card.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/utils/colors.dart';
+import 'package:notes_app/utils/text_styles.dart';
+
+class NoteCategoryCard extends StatefulWidget {
+
+  final String noteTitle;
+  final String noteContent;
+
+  final Future Function() removeNote;
+  final Future Function() editNote;
+  const NoteCategoryCard({
+    super.key, 
+    required this.noteTitle, 
+    required this.noteContent,
+     required this.removeNote, 
+     required this.editNote
+    });
+
+  @override
+  State<NoteCategoryCard> createState() => _NoteCategoryCardState();
+}
+
+class _NoteCategoryCardState extends State<NoteCategoryCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: AppColors.kCardColor,
+      child: Padding(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                //edit button
+                IconButton(
+                  onPressed: () {
+                    
+                  }, 
+                  icon: Icon(
+                    Icons.edit_outlined,
+                    color: AppColors.kWhiteColor,
+                  ),
+                ),
+
+                SizedBox(width: 10,),
+
+                //Delect Button
+                IconButton(
+                  onPressed: () {
+                  }, 
+                  icon: Icon(
+                    Icons.delete_outline,
+                    color: AppColors.kWhiteColor,
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              widget.noteTitle,
+              style: AppTextStyles.appSubtitle,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              widget.noteContent,
+              style: AppTextStyles.appDescriptionSmall.copyWith(
+                color: AppColors.kWhiteColor.withOpacity(0.5),
+              ),
+              maxLines: 6,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.left,
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
